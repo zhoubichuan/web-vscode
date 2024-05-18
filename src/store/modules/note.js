bd42b50e21e689d47b76449bf5a51c4bb98b2632
zhoubichuan <zhoubichuan@icloud.com>
2024-05-18 20:15:05 +0800

    更新yml

--- src/store/modules/note.js
+++ src/store/modules/note.js
@@ -28,10 +28,10 @@ export default {
         name: "Vue相关",
         items: [
           {
-            title: "web-vue2x",
+            title: "web-vscode",
             skill: "Vuepress",
-            img: "web-vue2x",
-            link: "/web-vue2x/",
+            img: "web-vscode",
+            link: "/web-vscode/",
           },
         ],
       },
--- src/util/request.js
+++ src/util/request.js
@@ -3,9 +3,9 @@ import { Message, MessageBox } from "element-ui";
 import { getToken } from "@/util/auth";
 
 // 创建axios实例
-let baseURL = "/web-vue2x/";
+let baseURL = "/web-vscode/";
 try {
-  baseURL = process ? process.env?.BASE_API : "/web-vue2x/";
+  baseURL = process ? process.env?.BASE_API : "/web-vscode/";
 } catch (e) {}
 const service = axios.create({
   baseURL,
